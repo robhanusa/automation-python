49129356044e068f3f1ac6646f4b176f24cd0588
Rob <rkhanusa@gmail.com>
2021-05-06 21:13:50 +0200

    updated file paths, made it handle .tiff images better

--- Image_flip_resize.py
+++ Image_flip_resize.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 30 06:08:08 2021
-
-@author: rkhan
-"""
+#!/usr/bin/env python3
 
 from PIL import Image
 import os
+import re
 
 permissions = 0o777 #the 0o prefix is for the octal numbering system
 
-image_path = r"C:\Users\rkhan\pythontest\courserafinalproject\images"
-project_path = r"C:\Users\rkhan\pythontest\courserafinalproject"
+image_path = r".\images"
+project_path = r"."
 
 #if the output folder doesn't already exist, create it
 if not os.path.exists(project_path+r"\output"):
@@ -19,7 +15,11 @@ if not os.path.exists(project_path+r"\output"):
 
 #rotate and resize images
 for i, image in enumerate(os.listdir(image_path)):
-    im = Image.open(image_path+"\\"+image)
-    im.rotate(270).resize((128,128)).save(project_path+"\output\\"+"flipped_and_resized{}.jpg".format(i))
+    #only handles images in .tiff or .jpg format
+    if re.search(r'.+(.{5})$',image).groups()[0] == '.tiff' or re.search(r'.+(.{4})$',image).groups()[0] == '.jpg':
+        im = Image.open(image_path+"\\"+image)
+        if re.search(r'.+(.{5})$',image).groups()[0] == '.tiff':
+            im = im.convert('RGB') #.tiff is RGBA, and JPG only support RGB
+        im.rotate(270).resize((128,128)).save(project_path+"\output\\"+"flipped_and_resized_{}.jpg".format(image))
+
     
-#I could improve this by making it a function with input of file path
